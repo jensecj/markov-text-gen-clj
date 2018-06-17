b374a4686f2730b7f754dcea7ebaf9c4c31e7ace
Jens Christian Jensen <jensecj@gmail.com>
2018-06-17 19:16:35 +0200

    added comments

--- src/markov_text_gen/core.clj
+++ src/markov_text_gen/core.clj
@@ -6,7 +6,10 @@
 ;; need this to spit big maps to file, dont print them to console
 (set! *print-length* nil)
 
-(defn- load-words-from-resource [file]
+(defn- load-words-from-resource
+  "Loads words from a file. Creates a vector of cleaned words by reading a file
+  from disk, and applying formatting."
+  [file]
   (println "loading " file)
   (assoc {:file file} :words
          (filter #(not (= % " " ""))
@@ -21,7 +24,9 @@
                      (s/replace #"[ ]+" " ")
                      (s/split #" ")))))
 
-(defn- create-markov-chain [state-size {words :words :as ctx}]
+(defn- create-markov-chain
+  "Creates a markov-chain of size STATE-SIZE, from the list of candidate WORDS."
+  [state-size {words :words :as ctx}]
   (let [part (partition state-size words)
         keys (map (partial s/join " ") part)
         values (rest (map #'first part))
@@ -30,21 +35,32 @@
         (dissoc :words)
         (assoc :state-size state-size :db (zipmap keys values)))))
 
-(defn- save-markov-chain-to-file [markov]
+(defn- save-markov-chain-to-file
+  "Saves a markov-chain to a file on disk."
+  [markov]
   (let [file (s/replace (:file markov) #".txt" ".markov")]
     (println "saving " (:file markov))
     (spit (str "markov-files/" file) (with-out-str (pr markov)))))
 
-(defn- load-markov-chain-from-file [file]
+(defn- load-markov-chain-from-file
+  "Loads a markov-chain from a file on disk."
+  [file]
   (println "loading " file)
   (read-string (slurp file)))
 
-(defn- merge-markov-chains [chains]
+(defn- merge-markov-chains
+  "Merges several markov-chains together into a single chain. if a key has
+  duplicate values they are perserved by making the value for that key a vector
+  of those."
+  [chains]
   (->> chains
        (pmap :db)
        (reduce #(merge-with (comp flatten vector) %1 %2))))
 
-(defn- generate [iterations initial-words markov-chain]
+(defn- generate
+  "tries to generate a sentence starting with INITAL-WORDS, taking ITERATIONS, and
+  using MARKOV-CHAIN as the source of new content."
+  [iterations initial-words markov-chain]
   (let [story (atom (into [] (s/split initial-words #" ")))]
     (dotimes [i iterations]
       (let [db (:db markov-chain)
@@ -53,9 +69,11 @@
             pattern (s/join " " items)
             result (get db pattern "")]
         (cond
+          ;; if there are multiple choices in the chain for some input, pick a random one
           (seq? result) (swap! story conj (rand-nth result))
           (not (empty? result)) (swap! story conj result)
-          :else nil)))
+          :else nil ;; short-circuits if there is not match in the markov chain.
+          )))
     (s/join " " @story)))
 
 (defn -main [& args]
